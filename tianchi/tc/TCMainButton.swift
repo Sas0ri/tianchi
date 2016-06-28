60bf17f512f28effb7c7ec373ea92b892079b5a7
Sasori <Sasori@SasorideMacBook-Pro.local>
2016-06-28 14:22:03 +0800

    UI 调整

---
+++ tianchi/tc/TCMainButton.swift
@@ -0,0 +1,21 @@
+//
+//  TCMainButton.swift
+//  tc
+//
+//  Created by Sasori on 16/6/28.
+//  Copyright © 2016年 Sasori. All rights reserved.
+//
+
+import UIKit
+
+class TCMainButton: UIButton {
+
+    /*
+    // Only override drawRect: if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func drawRect(rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
