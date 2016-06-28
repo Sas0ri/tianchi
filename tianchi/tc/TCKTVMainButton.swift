94904233fe7aa7f53275540d2f2329ddb32e1f3b
Sasori <Sasori@SasorideMacBook-Pro.local>
2016-06-28 15:54:25 +0800

    UI updated

---
+++ tianchi/tc/TCKTVMainButton.swift
@@ -0,0 +1,21 @@
+//
+//  TCKTVMainButton.swift
+//  tc
+//
+//  Created by Sasori on 16/6/28.
+//  Copyright © 2016年 Sasori. All rights reserved.
+//
+
+import UIKit
+
+class TCKTVMainButton: UIButton {
+
+    /*
+    // Only override drawRect: if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func drawRect(rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
